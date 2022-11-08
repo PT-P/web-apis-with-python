8e92d175bccc3932411392ed7048763b39d4b902
Laksamana Vixell T H <laksamanavixell@gmail.com>
2022-11-08 17:51:54 +0700

    app.py

--- app.py
+++ app.py
@@ -4,14 +4,59 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Define what the app does
-@app.get("/greet")
+@app.get("/data")
 def index():
-    """
-    TODO:
-    1. Capture first name & last name
-    2. If either is not provided: respond with an error
-    3. If first name is not provided and second name is provided: respond with "Hello Mr <second-name>!"
-    4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
-    5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
-    """
-    return jsonify("TODO")
+
+    name = request.args.get("nama")
+
+    thoriq = {
+        "nama": "Thoriq",
+        "umur": 20,
+        "ttl": "8 mei 2002",
+        "sekolah": {
+            "sma": {
+                "nama": "SMAN 1 BOGOR",
+                "lokasi": "jl selot"
+            },
+            "kuliah": {
+                "nama": "ITB",
+                "lokasi": "jl ganesha"
+            }
+        }
+    }
+
+    vixell = {
+        "nama": "Vixell",
+        "umur": 20,
+        "ttl": "18 juni 2002",
+        "sekolah": {
+            "sma": {
+                "nama": "SMA KOLESE LOYOLA",
+                "lokasi": "jl karanganyar"
+            },
+            "kuliah": {
+                "nama": "ITB",
+                "lokasi": "jl ganesha"
+            }
+        }
+    }
+    response = {
+        "status": "failed to obtain data"
+    }
+
+    if (not name) or (name == "thoriq-vixell" or name == "vixell-thoriq"):
+        response = {
+            "data": [thoriq, vixell]
+        }
+
+    elif name == "thoriq":
+        response = {
+            "data": thoriq
+        }
+
+    elif name == "vixell":
+        response = {
+            "data": vixell
+        }
+
+    return jsonify(response)
